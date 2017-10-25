2d719d22450fa6a3e9d5feaadb12400ffee0bfa5
Kalpit Mantri <kalpit.mantri1798@gmail.com>
2017-10-25 15:42:52 +0530

    Update stack.java

--- myStack/src/stack.java
+++ myStack/src/stack.java
@@ -2,10 +2,10 @@
 public interface stack {
 
 	
-	public void Push(String str);
-	public String Pop();
-	public String Top();
-	public boolean Isempty();
-	public boolean Isfull();
+	public void Push(String str); // Method to push some data in stack
+	public String Pop();  //Method to take out data from top of the stack
+	public String Top();  //Method to get the top element of the stack
+	public boolean Isempty();  //Method to check whether the stack is empty or not
+	public boolean Isfull();  //Method to check whether the stack is full or not
 	
 }
